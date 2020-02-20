80af462d19c282f42f9678ce0d987ba1a0c68513
Vincent Sanders <vincesanders@hotmail.com>
2020-02-20 17:12:59 -0600

    Added get recipes endpoint.

--- recipes/recipe-router.js
+++ recipes/recipe-router.js
@@ -4,4 +4,14 @@ const recipe = require('./recipe-model');
 
 const router = express.Router();
 
+router.get('/', (req, res) => {
+    recipe.getRecipes()
+    .then(recipes => {
+        res.json(recipes);
+    })
+    .catch(err => {
+        res.status(500).json({ message: 'Failed to get recipes' });
+    });
+});
+
 module.exports = router;
